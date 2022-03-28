91d17a9c34620400f934b3e6752513d8b53543ed
luckygith <102515829+luckygith@users.noreply.github.com>
2022-03-28 15:54:11 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>project</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="./images/4f.jpeg" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <video src="images/video.mp4" class="vi-bg" autoplay loop muted></video>
+        <div class="nav">
+            <div class="logo"><img src="1.jpeg" alt="hello" title="we"></div>
+            <ul class="menu">
+                
+                
+                <li><a href="userlogin.php">Login</li>
+                
+                <li><a href="adminlogin.php">Admin</li>
+</ul>
+       
+
+        </div>
+        <div>
+        
+</div>
+<div class="banner">
+
+    <a href="userlogin.php" class="btn">LOGIN</a>
+    <h2>Login Please</h2>
+</div>
+
+
+</header>
+</body>
+</html>
